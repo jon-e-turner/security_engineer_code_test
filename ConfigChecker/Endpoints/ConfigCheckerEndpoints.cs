803ef6c3c57a5f64b9b23ef499ade5823fa9deb8
Jon Turner <jon.e.turner@outlook.com>
2025-02-10 00:30:09 -0800

    Using Node.js with Express and Pug to build a quick front-end. Still to add: client-side validation logic, report display, how to pop-up a modal with the report. Need to probably look at the defualt style sheet, too.

--- ConfigChecker/Endpoints/ConfigCheckerEndpoints.cs
+++ ConfigChecker/Endpoints/ConfigCheckerEndpoints.cs
@@ -12,10 +12,6 @@ namespace ConfigChecker.Endpoints
   {
     public static void MapConfigCheckerEndpoints(this IEndpointRouteBuilder endpointRouteBuilder)
     {
-      _ = endpointRouteBuilder.MapGet("/", () => Results.Redirect("/upload"));
-
-      _ = endpointRouteBuilder.MapGet("/upload", () => Results.Ok());
-
       _ = endpointRouteBuilder.MapPost("/upload", async (IFormFile file, IFileUploadService uploadService, [FromServices] ChannelWriter<ProcessingRequestDto> processingChannel) =>
       {
         string path = string.Empty;
