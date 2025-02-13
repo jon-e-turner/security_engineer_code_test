00ba9ba0021998288e94e44643c5ff625119e788
Jon Turner <jon.e.turner@outlook.com>
2025-02-13 15:41:50 -0800

    Added some unit tests.
    Only a couple so far as I need to debug.

---
+++ ConfigCheckerTests/Dtos/ResourceDtoExtensionsTests.cs
@@ -0,0 +1,47 @@
+using ConfigChecker.DTOs;
+using ConfigChecker.Models;
+using System.Text.Json;
+
+namespace ConfigCheckerTests.Dtos;
+
+[TestClass]
+public class ResourceDtoExtensionsTests
+{
+    private readonly ResourceDto? testResource = JsonSerializer.Deserialize<ResourceDto>("""
+        {
+          "type": "database",
+          "name": "db1",
+          "open_ports": [22, 80],
+          "password": "supersecurepassword",
+          "encryption": true,
+          "mfa_enabled": true,
+          "azure_specific": {
+            "resource_group": "rg2",
+            "location": "westus",
+            "db_service": "Azure SQL Database"
+          }
+        }
+        """,
+        JsonSerializerOptions.Web);
+
+    [TestMethod]
+    public void CreateExposedPortFindingReturnsFindingDto()
+    {
+        if (testResource is not null)
+        {
+            var finding = testResource.CreateExposedPortFinding(80);
+
+            Assert.IsNotNull(finding);
+            Assert.AreEqual(testResource.Name, finding.ResourceName);
+            Assert.AreEqual("OpenPort", finding.Name);
+            Assert.Contains("Port 80 ", finding.Description);
+            Assert.Contains(" network groups ", finding.Mitigation);
+            Assert.AreEqual(Finding.SeverityRating.High.ToString(), finding.Severity);
+            Assert.AreEqual("862", finding.CweId);
+        }
+        else
+        {
+            Assert.Fail("Could not parse test resource");
+        }
+    }
+}
---
+++ ConfigCheckerTests/Dtos/ResourceDtoTests.cs
@@ -0,0 +1,54 @@
+﻿using ConfigChecker.DTOs;
+using System.Text.Json;
+
+namespace ConfigCheckerTests.Dtos
+{
+    [TestClass]
+    public class ResourceDtoTests
+    {
+        [TestMethod]
+        public void ResourceDtoShouldParseDatabaseJsonIntoCorrectResourceDto()
+        {
+            ResourceDto? testResource = JsonSerializer.Deserialize<ResourceDto>("""
+                {
+                  "type": "database",
+                  "name": "db1",
+                  "open_ports": [22, 80],
+                  "password": "supersecurepassword",
+                  "encryption": true,
+                  "mfa_enabled": true,
+                  "azure_specific": {
+                    "resource_group": "rg2",
+                    "location": "westus",
+                    "db_service": "Azure SQL Database"
+                  }
+                }
+                """,
+                JsonSerializerOptions.Web);
+
+            Assert.IsNotNull(testResource);
+
+            Assert.AreEqual("database", testResource.Type);
+            Assert.AreEqual("db1", testResource.Name);
+            
+            Assert.IsNotEmpty(testResource.AzureSpecific);
+            Assert.HasCount(3, testResource.AzureSpecific);
+            Assert.AreEqual("rg2", testResource.AzureSpecific["resource_group"]);
+            Assert.AreEqual("westus", testResource.AzureSpecific["location"]);
+            Assert.AreEqual("Azure SQL Database", testResource.AzureSpecific["db_service"]);
+            
+            Assert.IsNotNull(testResource.SecuritySettings);
+            Assert.IsNotEmpty(testResource.SecuritySettings);
+            Assert.HasCount(4, testResource.SecuritySettings);
+
+            var ports = testResource.SecuritySettings["open_ports"].Deserialize<int[]>();
+            Assert.IsNotNull(ports);
+            Assert.Contains(22, ports);
+            Assert.Contains(80, ports);
+
+            Assert.AreEqual("supersecurepassword", testResource.SecuritySettings["password"].GetString());
+            Assert.IsTrue(testResource.SecuritySettings["encryption"].GetBoolean());
+            Assert.IsTrue(testResource.SecuritySettings["mfa_enabled"].GetBoolean());
+        }
+    }
+}
