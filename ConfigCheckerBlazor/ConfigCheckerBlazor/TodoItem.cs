d9cf92132bdaaa2b8a3d64380a9969035a72d14b
Jon Turner <jon.e.turner@outlook.com>
2025-05-03 21:54:27 -0700

    Working through a learn Blazor tutorial.
    - Added Todo page
    - Added Todo page to NavMenu

---
+++ ConfigCheckerBlazor/ConfigCheckerBlazor/TodoItem.cs
@@ -0,0 +1,17 @@
+namespace ConfigCheckerBlazor
+{
+    public class TodoItem
+    {
+        public string? Title
+        {
+            get;
+            set;
+        }
+
+        public bool IsDone
+        {
+            get;
+            set;
+        } = false;
+    }
+}
