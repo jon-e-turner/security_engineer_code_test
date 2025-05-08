bf183af9c3773c2a5ba4e8098748e26683e729d4
Jon Turner <jon.e.turner@outlook.com>
2025-05-07 23:13:15 -0700

    Auto-refactor because I touched them.

--- ConfigCheckerTests/Dtos/FindingsFactoryTests.cs
+++ ConfigCheckerTests/Dtos/FindingsFactoryTests.cs
@@ -1,131 +1,135 @@
-using ConfigChecker.Dtos;
 using System.Text.Json;
 
-namespace ConfigCheckerTests.Dtos;
+using ConfigChecker.Dtos;
 
-[TestClass]
-public class FindingsFactoryTests
+using ConfigCheckerTests.TestHarness;
+
+namespace ConfigCheckerTests.Dtos
 {
-    private readonly ResourceDto? testResource = JsonSerializer.Deserialize<ResourceDto>("""
-        {
-          "type": "database",
-          "name": "db1",
-          "open_ports": [22, 80],
-          "password": "supersecurepassword",
-          "encryption": true,
-          "mfa_enabled": true,
-          "azure_specific": {
-            "resource_group": "rg2",
-            "location": "westus",
-            "db_service": "Azure SQL Database"
-          }
-        }
-        """
-    ,
-    TestHarness.Options.SerializerOptions);
-
-    [TestMethod]
-    public void CreateExposedPortFindingReturnsFindingDto()
+    [ TestClass ]
+    public class FindingsFactoryTests
     {
-        if (testResource is not null)
-        {
-            var finding = testResource.CreateExposedPortFinding(80);
+        private readonly ResourceDto? testResource = JsonSerializer.Deserialize<ResourceDto>("""
+                {
+                  "type": "database",
+                  "name": "db1",
+                  "open_ports": [22, 80],
+                  "password": "supersecurepassword",
+                  "encryption": true,
+                  "mfa_enabled": true,
+                  "azure_specific": {
+                    "resource_group": "rg2",
+                    "location": "westus",
+                    "db_service": "Azure SQL Database"
+                  }
+                }
+                """
+            ,
+            Options.SerializerOptions);
 
-            Assert.IsNotNull(finding);
-            Assert.AreEqual(testResource.Name, finding.ResourceName);
-            Assert.AreEqual(FindingName.OpenPort, finding.Name);
-            Assert.Contains("Port 80 ", finding.Description);
-            Assert.Contains(" network groups ", finding.Mitigation);
-            Assert.AreEqual(FindingSeverity.High, finding.Severity);
-            Assert.AreEqual("862", finding.CweId);
-        }
-        else
+        [ TestMethod ]
+        public void CreateExposedPortFindingReturnsFindingDto()
         {
-            Assert.Fail("Could not parse test resource");
-        }
-    }
+            if (testResource is not null)
+            {
+                var finding = testResource.CreateExposedPortFinding(80);
 
-    [TestMethod]
-    public void CreateExposedRcePortFindingReturnsFindingDto()
-    {
-        if (testResource is not null)
-        {
-            var finding = testResource.CreateExposedRcePortFinding(22);
-
-            Assert.IsNotNull(finding);
-            Assert.AreEqual(testResource.Name, finding.ResourceName);
-            Assert.AreEqual(FindingName.OpenRcePort, finding.Name);
-            Assert.Contains("Port 22 ", finding.Description);
-            Assert.Contains(" network groups ", finding.Mitigation);
-            Assert.AreEqual(FindingSeverity.Critical, finding.Severity);
-            Assert.AreEqual("862", finding.CweId);
+                Assert.IsNotNull(finding);
+                Assert.AreEqual(testResource.Name, finding.ResourceName);
+                Assert.AreEqual(FindingName.OpenPort, finding.Name);
+                Assert.Contains("Port 80 ", finding.Description);
+                Assert.Contains(" network groups ", finding.Mitigation);
+                Assert.AreEqual(FindingSeverity.High, finding.Severity);
+                Assert.AreEqual("862", finding.CweId);
+            }
+            else
+            {
+                Assert.Fail("Could not parse test resource");
+            }
         }
-        else
-        {
-            Assert.Fail("Could not parse test resource");
-        }
-    }
 
-    [TestMethod]
-    public void CreateWeakPasswordFindingReturnsFindingDto()
-    {
-        if (testResource is not null)
+        [ TestMethod ]
+        public void CreateExposedRcePortFindingReturnsFindingDto()
         {
-            var finding = testResource.CreateWeakPasswordFinding();
+            if (testResource is not null)
+            {
+                var finding = testResource.CreateExposedRcePortFinding(22);
 
-            Assert.IsNotNull(finding);
-            Assert.AreEqual(testResource.Name, finding.ResourceName);
-            Assert.AreEqual(FindingName.WeakPassword, finding.Name);
-            Assert.Contains(" minimum requirements.", finding.Description);
-            Assert.Contains(" length and complexity.", finding.Mitigation);
-            Assert.AreEqual(FindingSeverity.Critical, finding.Severity);
-            Assert.AreEqual("1391", finding.CweId);
+                Assert.IsNotNull(finding);
+                Assert.AreEqual(testResource.Name, finding.ResourceName);
+                Assert.AreEqual(FindingName.OpenRcePort, finding.Name);
+                Assert.Contains("Port 22 ", finding.Description);
+                Assert.Contains(" network groups ", finding.Mitigation);
+                Assert.AreEqual(FindingSeverity.Critical, finding.Severity);
+                Assert.AreEqual("862", finding.CweId);
+            }
+            else
+            {
+                Assert.Fail("Could not parse test resource");
+            }
         }
-        else
-        {
-            Assert.Fail("Could not parse test resource");
-        }
-    }
 
-    [TestMethod]
-    public void CreateMfaDisabledFindingReturnsFindingDto()
-    {
-        if (testResource is not null)
+        [ TestMethod ]
+        public void CreateWeakPasswordFindingReturnsFindingDto()
         {
-            var finding = testResource.CreateMfaDisabledFinding();
+            if (testResource is not null)
+            {
+                var finding = testResource.CreateWeakPasswordFinding();
 
-            Assert.IsNotNull(finding);
-            Assert.AreEqual(testResource.Name, finding.ResourceName);
-            Assert.AreEqual(FindingName.MfaDisabled, finding.Name);
-            Assert.Contains("Multi-factor authentication ", finding.Description);
-            Assert.Contains("Require multi-factor ", finding.Mitigation);
-            Assert.AreEqual(FindingSeverity.High, finding.Severity);
-            Assert.AreEqual("308", finding.CweId);
+                Assert.IsNotNull(finding);
+                Assert.AreEqual(testResource.Name, finding.ResourceName);
+                Assert.AreEqual(FindingName.WeakPassword, finding.Name);
+                Assert.Contains(" minimum requirements.", finding.Description);
+                Assert.Contains(" length and complexity.", finding.Mitigation);
+                Assert.AreEqual(FindingSeverity.Critical, finding.Severity);
+                Assert.AreEqual("1391", finding.CweId);
+            }
+            else
+            {
+                Assert.Fail("Could not parse test resource");
+            }
         }
-        else
-        {
-            Assert.Fail("Could not parse test resource");
-        }
-    }
 
-    [TestMethod]
-    public void CreateEncryptionDisabledFindingReturnsFindingDto()
-    {
-        if (testResource is not null)
+        [ TestMethod ]
+        public void CreateMfaDisabledFindingReturnsFindingDto()
         {
-            var finding = testResource.CreateEncryptionDisabledFinding();
+            if (testResource is not null)
+            {
+                var finding = testResource.CreateMfaDisabledFinding();
 
-            Assert.IsNotNull(finding);
-            Assert.AreEqual(testResource.Name, finding.ResourceName);
-            Assert.AreEqual(FindingName.EncryptionDisabled, finding.Name);
-            Assert.Contains("Encryption-at-rest ", finding.Description);
-            Assert.Contains("Require encryption ", finding.Mitigation);
-            Assert.AreEqual(FindingSeverity.High, finding.Severity);
-            Assert.AreEqual("312", finding.CweId);
+                Assert.IsNotNull(finding);
+                Assert.AreEqual(testResource.Name, finding.ResourceName);
+                Assert.AreEqual(FindingName.MfaDisabled, finding.Name);
+                Assert.Contains("Multi-factor authentication ", finding.Description);
+                Assert.Contains("Require multi-factor ", finding.Mitigation);
+                Assert.AreEqual(FindingSeverity.High, finding.Severity);
+                Assert.AreEqual("308", finding.CweId);
+            }
+            else
+            {
+                Assert.Fail("Could not parse test resource");
+            }
         }
-        else
+
+        [ TestMethod ]
+        public void CreateEncryptionDisabledFindingReturnsFindingDto()
         {
-            Assert.Fail("Could not parse test resource");
+            if (testResource is not null)
+            {
+                var finding = testResource.CreateEncryptionDisabledFinding();
+
+                Assert.IsNotNull(finding);
+                Assert.AreEqual(testResource.Name, finding.ResourceName);
+                Assert.AreEqual(FindingName.EncryptionDisabled, finding.Name);
+                Assert.Contains("Encryption-at-rest ", finding.Description);
+                Assert.Contains("Require encryption ", finding.Mitigation);
+                Assert.AreEqual(FindingSeverity.High, finding.Severity);
+                Assert.AreEqual("312", finding.CweId);
+            }
+            else
+            {
+                Assert.Fail("Could not parse test resource");
+            }
         }
     }
 }
--- ConfigCheckerTests/Dtos/ResourceDtoTests.cs
+++ ConfigCheckerTests/Dtos/ResourceDtoTests.cs
@@ -1,30 +1,33 @@
-﻿using ConfigChecker.Dtos;
-using System.Text.Json;
+﻿using System.Text.Json;
+
+using ConfigChecker.Dtos;
+
+using ConfigCheckerTests.TestHarness;
 
 namespace ConfigCheckerTests.Dtos
 {
-    [TestClass]
+    [ TestClass ]
     public class ResourceDtoTests
     {
-        [TestMethod]
+        [ TestMethod ]
         public void ResourceDtoShouldParseDatabaseJsonIntoCorrectResourceDto()
         {
-            ResourceDto? testResource = JsonSerializer.Deserialize<ResourceDto>("""
-                {
-                  "type": "database",
-                  "name": "db1",
-                  "open_ports": [22, 80],
-                  "password": "supersecurepassword",
-                  "encryption": true,
-                  "mfa_enabled": true,
-                  "azure_specific": {
-                    "resource_group": "rg2",
-                    "location": "westus",
-                    "db_service": "Azure SQL Database"
-                  }
-                }
-                """,
-                TestHarness.Options.SerializerOptions);
+            var testResource = JsonSerializer.Deserialize<ResourceDto>("""
+                                                                       {
+                                                                         "type": "database",
+                                                                         "name": "db1",
+                                                                         "open_ports": [22, 80],
+                                                                         "password": "supersecurepassword",
+                                                                         "encryption": true,
+                                                                         "mfa_enabled": true,
+                                                                         "azure_specific": {
+                                                                           "resource_group": "rg2",
+                                                                           "location": "westus",
+                                                                           "db_service": "Azure SQL Database"
+                                                                         }
+                                                                       }
+                                                                       """,
+                Options.SerializerOptions);
 
             Assert.IsNotNull(testResource);
 
@@ -33,43 +36,43 @@ namespace ConfigCheckerTests.Dtos
 
             Assert.IsNotEmpty(testResource.AzureSpecific);
             Assert.HasCount(3, testResource.AzureSpecific);
-            Assert.AreEqual("rg2", testResource.AzureSpecific["resource_group"]);
-            Assert.AreEqual("westus", testResource.AzureSpecific["location"]);
-            Assert.AreEqual("Azure SQL Database", testResource.AzureSpecific["db_service"]);
+            Assert.AreEqual("rg2", testResource.AzureSpecific[ "resource_group" ]);
+            Assert.AreEqual("westus", testResource.AzureSpecific[ "location" ]);
+            Assert.AreEqual("Azure SQL Database", testResource.AzureSpecific[ "db_service" ]);
 
             Assert.IsNotNull(testResource.SecuritySettings);
             Assert.IsNotEmpty(testResource.SecuritySettings);
             Assert.HasCount(4, testResource.SecuritySettings);
 
-            var ports = testResource.SecuritySettings["open_ports"].Deserialize<int[]>();
+            var ports = testResource.SecuritySettings[ "open_ports" ].Deserialize<int[ ]>();
             Assert.IsNotNull(ports);
             Assert.Contains(22, ports);
             Assert.Contains(80, ports);
 
-            Assert.AreEqual("supersecurepassword", testResource.SecuritySettings["password"].GetString());
-            Assert.IsTrue(testResource.SecuritySettings["encryption"].GetBoolean());
-            Assert.IsTrue(testResource.SecuritySettings["mfa_enabled"].GetBoolean());
+            Assert.AreEqual("supersecurepassword", testResource.SecuritySettings[ "password" ].GetString());
+            Assert.IsTrue(testResource.SecuritySettings[ "encryption" ].GetBoolean());
+            Assert.IsTrue(testResource.SecuritySettings[ "mfa_enabled" ].GetBoolean());
         }
 
-        [TestMethod]
+        [ TestMethod ]
         public void ResourceDtoShouldParseVirtualMachineJsonIntoCorrectResourceDto()
         {
-            ResourceDto? testResource = JsonSerializer.Deserialize<ResourceDto>("""
-                {
-                  "type": "virtual_machine",
-                  "name": "vm1",
-                  "open_ports": [22, 80, 443],
-                  "password": "weakpassword",
-                  "encryption": false,
-                  "mfa_enabled": false,
-                  "azure_specific": {
-                    "resource_group": "rg1",
-                    "location": "eastus",
-                    "vm_size": "Standard_DS1_v2"
-                  }
-                }
-                """,
-                TestHarness.Options.SerializerOptions);
+            var testResource = JsonSerializer.Deserialize<ResourceDto>("""
+                                                                       {
+                                                                         "type": "virtual_machine",
+                                                                         "name": "vm1",
+                                                                         "open_ports": [22, 80, 443],
+                                                                         "password": "weakpassword",
+                                                                         "encryption": false,
+                                                                         "mfa_enabled": false,
+                                                                         "azure_specific": {
+                                                                           "resource_group": "rg1",
+                                                                           "location": "eastus",
+                                                                           "vm_size": "Standard_DS1_v2"
+                                                                         }
+                                                                       }
+                                                                       """,
+                Options.SerializerOptions);
 
             Assert.IsNotNull(testResource);
 
@@ -78,42 +81,42 @@ namespace ConfigCheckerTests.Dtos
 
             Assert.IsNotEmpty(testResource.AzureSpecific);
             Assert.HasCount(3, testResource.AzureSpecific);
-            Assert.AreEqual("rg1", testResource.AzureSpecific["resource_group"]);
-            Assert.AreEqual("eastus", testResource.AzureSpecific["location"]);
-            Assert.AreEqual("Standard_DS1_v2", testResource.AzureSpecific["vm_size"]);
+            Assert.AreEqual("rg1", testResource.AzureSpecific[ "resource_group" ]);
+            Assert.AreEqual("eastus", testResource.AzureSpecific[ "location" ]);
+            Assert.AreEqual("Standard_DS1_v2", testResource.AzureSpecific[ "vm_size" ]);
 
             Assert.IsNotNull(testResource.SecuritySettings);
             Assert.IsNotEmpty(testResource.SecuritySettings);
             Assert.HasCount(4, testResource.SecuritySettings);
 
-            var ports = testResource.SecuritySettings["open_ports"].Deserialize<int[]>();
+            var ports = testResource.SecuritySettings[ "open_ports" ].Deserialize<int[ ]>();
             Assert.IsNotNull(ports);
             Assert.Contains(22, ports);
             Assert.Contains(80, ports);
             Assert.Contains(443, ports);
 
-            Assert.AreEqual("weakpassword", testResource.SecuritySettings["password"].GetString());
-            Assert.IsFalse(testResource.SecuritySettings["encryption"].GetBoolean());
-            Assert.IsFalse(testResource.SecuritySettings["mfa_enabled"].GetBoolean());
+            Assert.AreEqual("weakpassword", testResource.SecuritySettings[ "password" ].GetString());
+            Assert.IsFalse(testResource.SecuritySettings[ "encryption" ].GetBoolean());
+            Assert.IsFalse(testResource.SecuritySettings[ "mfa_enabled" ].GetBoolean());
         }
 
-        [TestMethod]
+        [ TestMethod ]
         public void ResourceDtoShouldParseStorageAccountJsonIntoCorrectResourceDto()
         {
-            ResourceDto? testResource = JsonSerializer.Deserialize<ResourceDto>("""
-                {
-                  "type": "storage_account",
-                  "name": "storage1",
-                  "encryption": false,
-                  "azure_specific": {
-                    "resource_group": "rg1",
-                    "location": "eastus",
-                    "account_tier": "Standard",
-                    "replication": "LRS"
-                  }
-                }
-                """,
-                TestHarness.Options.SerializerOptions);
+            var testResource = JsonSerializer.Deserialize<ResourceDto>("""
+                                                                       {
+                                                                         "type": "storage_account",
+                                                                         "name": "storage1",
+                                                                         "encryption": false,
+                                                                         "azure_specific": {
+                                                                           "resource_group": "rg1",
+                                                                           "location": "eastus",
+                                                                           "account_tier": "Standard",
+                                                                           "replication": "LRS"
+                                                                         }
+                                                                       }
+                                                                       """,
+                Options.SerializerOptions);
 
             Assert.IsNotNull(testResource);
 
@@ -122,16 +125,16 @@ namespace ConfigCheckerTests.Dtos
 
             Assert.IsNotEmpty(testResource.AzureSpecific);
             Assert.HasCount(4, testResource.AzureSpecific);
-            Assert.AreEqual("rg1", testResource.AzureSpecific["resource_group"]);
-            Assert.AreEqual("eastus", testResource.AzureSpecific["location"]);
-            Assert.AreEqual("Standard", testResource.AzureSpecific["account_tier"]);
-            Assert.AreEqual("LRS", testResource.AzureSpecific["replication"]);
+            Assert.AreEqual("rg1", testResource.AzureSpecific[ "resource_group" ]);
+            Assert.AreEqual("eastus", testResource.AzureSpecific[ "location" ]);
+            Assert.AreEqual("Standard", testResource.AzureSpecific[ "account_tier" ]);
+            Assert.AreEqual("LRS", testResource.AzureSpecific[ "replication" ]);
 
             Assert.IsNotNull(testResource.SecuritySettings);
             Assert.IsNotEmpty(testResource.SecuritySettings);
             Assert.HasCount(1, testResource.SecuritySettings);
 
-            Assert.IsFalse(testResource.SecuritySettings["encryption"].GetBoolean());
+            Assert.IsFalse(testResource.SecuritySettings[ "encryption" ].GetBoolean());
         }
     }
 }
